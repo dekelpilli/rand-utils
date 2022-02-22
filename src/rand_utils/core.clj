6b551a1f7ca9a61dc9650551d4f5a7ca043552bf
Dekel Pilli <dekelpilli@gmail.com>
2022-02-23 00:08:05 +1100

    Refactor alias method init to create a function instead of data to be passed around

--- src/rand_utils/core.clj
+++ src/rand_utils/core.clj
@@ -1,46 +1,47 @@
 (ns rand-utils.core)
 
-;https://www.keithschwarz.com/darts-dice-coins/
-(defn alias-init [probabilities]
-  "Performs the initialisation for Vose's Alias Method"
-  (let [n (count probabilities)
-        base (vec (repeat n nil))
-        scaled-probs (mapv #(* n %) probabilities)
-        {:keys [small large]} (reduce
-                                (fn [acc i]
-                                  (let [p (nth scaled-probs i)]
-                                    (update acc (if (< p 1) :small :large) conj i)))
-                                {:small [] :large []}
-                                (range n))]
-    (loop [small small
-           large large
-           alias base
-           prob base]
-      (cond
-        (and (seq small) (seq large)) (let [l (first small)
-                                            p-of-l (nth scaled-probs l)
-                                            g (first large)
-                                            p-of-g (- (+ (nth scaled-probs g) p-of-l)
-                                                      1)
-                                            [small large] (if (< p-of-g 1)
-                                                            [(conj small g) large]
-                                                            [small (conj large g)])]
-                                        (recur (rest small)
-                                               (rest large)
-                                               (assoc alias l g)
-                                               (assoc prob l p-of-l)))
-        (seq large) (recur nil (rest large) alias (assoc prob (first large) 1))
-        (seq small) (recur (rest small) nil alias (assoc prob (first small) 1))
-        :else {:prob prob :alias alias}))))
-
-(defn alias-gen [{:keys [prob alias]} values]
-  (let [i (rand-int (count prob))
-        heads? (<= (rand) (nth prob i))]
-    (nth values (if heads? i (nth alias i)))))
+(defn alias-generator
+  "Performs the initialisation for Vose's Alias Method and returns a function that generates values based on the probabilities"
+  ([probabilities-map] (alias-generator (keys probabilities-map) (vals probabilities-map)))
+  ([values probabilities]
+   (let [values (vec values)
+         n (count probabilities)
+         base (vec (repeat n nil))
+         scaled-probs (mapv #(* n %) probabilities)
+         {:keys [small large]} (reduce
+                                 (fn [acc i]
+                                   (let [p (nth scaled-probs i)]
+                                     (update acc (if (< p 1) :small :large) conj i)))
+                                 {:small [] :large []}
+                                 (range n))]
+     (loop [small small
+            large large
+            alias base
+            prob base]
+       (cond
+         (and (seq small) (seq large)) (let [l (first small)
+                                             p-of-l (nth scaled-probs l)
+                                             g (first large)
+                                             p-of-g (- (+ (nth scaled-probs g) p-of-l)
+                                                       1)
+                                             [small large] (if (< p-of-g 1)
+                                                             [(conj small g) large]
+                                                             [small (conj large g)])]
+                                         (recur (rest small)
+                                                (rest large)
+                                                (assoc alias l g)
+                                                (assoc prob l p-of-l)))
+         (seq large) (recur nil (rest large) alias (assoc prob (first large) 1))
+         (seq small) (recur (rest small) nil alias (assoc prob (first small) 1))
+         :else #(let [i (rand-int n)]
+                  (nth values (if (<= (rand) (nth prob i))
+                                i
+                                (nth alias i)))))))))
 
 (comment
-  (-> (alias-init [2/10 5/10 3/10])
-      (alias-gen ["red" "green" "blue"])))
+  ((alias-generator ["orage" "yellow" "green" "cyan" "grey" "blue" "pink"]
+                    [1/8, 1/5, 1/10, 1/4, 1/10, 1/10, 1/8])) ;;TODO check - cyan too frequent, possibile bug?
+  ((alias-generator {"red" 1/5, "green" 1/2, "blue" 3/10})))
 
 (defn weightings->probabilities [ws]
   (let [total (reduce + 0 ws)]

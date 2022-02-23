92bf9e242f833e49b4c5adc4521aa7c265cf0464
Dekel Pilli <dekelpilli@gmail.com>
2022-02-24 10:39:54 +1100

    Use data.dequeue library to maintain cljs support and immutability

--- src/rand_utils/core.clj
+++ src/rand_utils/core.clj
@@ -1,10 +1,10 @@
 (ns rand-utils.core
-  (:import (java.util ArrayDeque)
-           (org.apache.commons.rng.sampling.distribution AliasMethodDiscreteSampler)
+  (:require [data.deque :as dq])
+  (:import (org.apache.commons.rng.sampling.distribution AliasMethodDiscreteSampler)
            (org.apache.commons.rng.core.source32 JDKRandom)))
 
 (defn alias-generator
-  "Performs the initialisation for Vose's Alias Method and returns a function that generates values based on the weightings"
+  "Performs the initialisation for Vose's Alias Method and returns a function that generates values based on the weightings."
   ([weightings-map] (alias-generator (keys weightings-map) (vals weightings-map)))
   ([values weightings]
    (assert (= (count weightings) (count values)))
@@ -14,29 +14,35 @@
          total (reduce + weightings)
          avg (/ total n)
          base (vec (repeat n nil))
-         small (ArrayDeque.) ;TODO replace with clojure data structures for cljs support?
-         large (ArrayDeque.)]
-     (doseq [i (range n)]
-       (let [p (nth weightings i)]
-         (-> (if (>= p avg) large small)
-             (.add i))))
-     (loop [alias base
+         indexes (reduce
+                   (fn [acc i]
+                     (let [p (nth weightings i)]
+                       (update acc (if (>= p avg) :large :small) dq/add-first i)))
+                   {:large dq/EMPTY :small dq/EMPTY}
+                   (range n))]
+     (loop [weightings weightings
+            alias base
             probability base
-            weightings weightings]
+            {:keys [large small] :as indexes} indexes]
        (cond
-         (and (seq small) (seq large)) (let [less (.removeLast small)
-                                             more (.removeLast large)
+         (and (seq large) (seq small)) (let [less (dq/peek-last small)
+                                             more (dq/peek-last large)
                                              p-of-less (* n (nth weightings less))
                                              p-of-more (- (+ (nth weightings more) (nth weightings less))
                                                           avg)]
-                                         (if (>= p-of-more avg)
-                                           (.add large more)
-                                           (.add small more))
-                                         (recur (assoc alias less more)
+                                         (recur (assoc weightings more p-of-more)
+                                                (assoc alias less more)
                                                 (assoc probability less p-of-less)
-                                                (assoc weightings more p-of-more)))
-         (seq small) (recur alias (assoc probability (.removeLast small) total) weightings)
-         (seq large) (recur alias (assoc probability (.removeLast large) total) weightings)
+                                                (-> indexes
+                                                    (update :large dq/remove-last)
+                                                    (update :small dq/remove-last)
+                                                    (update (if (>= p-of-more avg) :large :small) dq/add-last more))))
+         (seq large) (recur weightings alias
+                            (assoc probability (dq/peek-last large) total)
+                            (update indexes :large dq/remove-last))
+         (seq small) (recur weightings alias
+                            (assoc probability (dq/peek-last small) total)
+                            (update indexes :small dq/remove-last))
          :else (letfn [(generate
                          ([] (generate rand))
                          ([rand-f] (let [i (int (* (rand-f) n))]
